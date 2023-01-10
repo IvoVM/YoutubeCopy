b78480f132d3f5c34a6cc39621bf201806d97b32
IvoVM <ivomastrangelo12@gmail.com>
2023-01-10 12:21:10 -0300

    Added SearchFeed and SearchBar Functionality

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,34 @@
-
-
-const SearchFeed= ()=>{
-    return(
-        <h2>search</h2>
-    )
-}
+import { useState, useEffect } from "react";
+import { Box, getBottomNavigationUtilityClass, Stack, Typography } from "@mui/material";
+import { Sidebar, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { useParams } from "react-router-dom";
+const SearchFeed = () => {
+  const [videos, setVideos] = useState(null);
+  const { searchTerm } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
+      setVideos(data.items);
+    });
+  }, [searchTerm]);
+  return (
+    <Box
+    p={2}
+    sx={{ overflowY: "auto", height: "90vh", flex: 2 }}
+    className="caja"
+  >
+    <Typography
+      variant="h4"
+      sx={{ color: "#fff" }}
+      fontWeight="bold"
+      mb={2}
+    >
+      Search results for:
+      <span style={{ color: "#f31503" }}>{searchTerm}</span>
+    </Typography>
+    <Videos videos={videos} />
+  </Box>
+  );
+};
 
 export default SearchFeed;
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,5 +1,5 @@
-
-
+import {useEffect,useState} from 'react';
+import { Link , useParams} from 'react-router-dom'
 const VideoDetail= ()=>{
     return(
         <h2>Video Detail</h2>
